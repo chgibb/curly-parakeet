3640d3de2830c1ad0d9cbf6522048d8300573b0d
Chris Gibb <chris.gibb@outlook.com>
2018-01-09 21:38:38 -0500

    moved open block keyword

--- src/req/sections/01.ts
+++ src/req/sections/01.ts
@@ -10,10 +10,10 @@ export class ZeroOne extends ICDSection
         this.regExp = /(01 Certain Infectious or Parasitic Diseases)/;
         this.tokenType = "icd11.SectionTopHeader";
         this.completionItem = {
-            label : "Start 01 Certain Infectious or Parasitic Diseases",
+            label : "01 Certain Infectious or Parasitic Diseases",
             kind : monaco.languages.CompletionItemKind.Function,
             documentation : "This chapter includes certain conditions caused by a pathogenic organism or microorganism, such as a bacterium, virus, parasite, or fungus.",
-            insertText : `Start 01 Certain Infectious or Parasitic Diseases${"\n"}   ${"\n"}End`
+            insertText : `01 Certain Infectious or Parasitic Diseases Start${"\n"}   ${"\n"}End`
         };
     }
 }
