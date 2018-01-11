fddc3c4a86718752877f06bc19a0fc7ea415ebfa
Chris Gibb <chris.gibb@outlook.com>
2018-01-11 15:09:58 -0500

    added test

---
+++ __tests__/autoComplete/01GastroFoodBorneItems.ts
@@ -0,0 +1,26 @@
+/// <reference types="ts-jest" />
+
+import {autoComplete} from "./../../src/req/autoComplete";
+import {Gastro} from "./../../src/req/sections/01/gastro";
+import {FoodBorne} from "./../../src/req/sections/01/foodBorne";
+
+let gastro = new Gastro();
+let foodBorne = new FoodBorne();
+
+it(`should auto complete gastro foodborne items`,() => {
+    let doc = [
+        "01 Certain Infectious or Parasitic Diseases Start",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "      Bacterial Foodborne Intoxications Start",
+        "            "
+    ].join("\n");
+
+    let res : void | monaco.languages.CompletionItem[] = autoComplete(doc);
+
+    expect((<monaco.languages.CompletionItem[]>res).length).toBe(foodBorne.childItems.length);
+
+    for(let i = 0; i != (<monaco.languages.CompletionItem[]>res).length; ++i)
+    {
+        expect((<monaco.languages.CompletionItem[]>res)[i].label).toBe(foodBorne.childItems[i].completionItem.label);
+    }
+});
