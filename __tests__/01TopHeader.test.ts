4bc1d89cd76b82618af2c10a9e2456bb57b589ab
Chris Gibb <chris.gibb@outlook.com>
2018-01-11 13:59:06 -0500

    added test

--- __tests__/01TopHeader.test.ts
+++ __tests__/01TopHeader.test.ts
@@ -9,3 +9,8 @@ it(`should auto complete chapter 1 top header`,() => {
     let res : void | monaco.languages.CompletionItem[] = autoComplete("c");
     expect((<monaco.languages.CompletionItem[]>res)[0].label).toBe(zeroOne.completionItem.label);
 });
+
+it(`should auto complete chapter 1 top header`,() => {
+    let res : void | monaco.languages.CompletionItem[] = autoComplete("01");
+    expect((<monaco.languages.CompletionItem[]>res)[0].label).toBe(zeroOne.completionItem.label);
+});
