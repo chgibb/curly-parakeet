43439d5904c742c464f34025a6134815de5309e6
Chris Gibb <chris.gibb@outlook.com>
2018-01-11 14:35:35 -0500

    added test

--- __tests__/autoComplete/01GastroChildren.test.ts
+++ __tests__/autoComplete/01GastroChildren.test.ts
@@ -2,8 +2,10 @@
 
 import {autoComplete} from "./../../src/req/autoComplete";
 import {Gastro} from "./../../src/req/sections/01/gastro";
+import {FoodBorne} from "./../../src/req/sections/01/foodBorne";
 
 let gastro = new Gastro();
+let foodBorne = new FoodBorne();
 
 it(`should auto complete gastro subsections`,() => {
     let document = 
@@ -22,5 +24,17 @@ it(`should auto complete gastro subsections`,() => {
     {
         expect((<monaco.languages.CompletionItem[]>res)[i].label).toBe(gastro.childSections[i].completionItem.label);
     }
+    
+});
+
+it(`should auto complete Bacterial Foodborne Intoxications`,() => {
+    let document = 
+    `01 Certain Infectious or Parasitic Diseases Start
+    Gastroenteritis and Colitis of Infectious Origin Start
+       foodborne
+    `;
+    let res : void | monaco.languages.CompletionItem[] = autoComplete(document);
+
+    expect((<monaco.languages.CompletionItem[]>res)[0].label).toBe(foodBorne.completionItem.label);
     
 });
