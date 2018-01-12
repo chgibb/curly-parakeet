2df357123fbb853cd348d8665d5c0c699b3e1775
Chris Gibb <chris.gibb@outlook.com>
2018-01-12 14:29:25 -0500

    more tests for block traversak

--- __tests__/AST/walkUpToParentBlock.test.ts
+++ __tests__/AST/walkUpToParentBlock.test.ts
@@ -6,8 +6,15 @@ import {
     ICDSection,
     findParentSectionFromLinePosition
 } from "./../../src/req/icdToken";
-import {ZeroOne} from "./../../src/req/sections/01";
 import {getTokenLayout} from "./../../src//req/treeLayout";
+import {ZeroOne} from "./../../src/req/sections/01";
+import {Gastro} from "./../../src/req/sections/01/gastro";
+import {FoodBorne} from "./../../src/req/sections/01/foodBorne";
+
+let zerOne = new ZeroOne();
+
+let gastro = new Gastro();
+let foodBorne = new FoodBorne();
 
 let zeroOne = new ZeroOne();
 
@@ -20,5 +27,97 @@ it(`should find chapter 1 header`,() => {
     ];
     let res = findParentSectionFromLinePosition(doc,doc.length-1,getTokenLayout());
     expect((<ICDSection>res).completionItem.label).toBe(zeroOne.completionItem.label);
+
+});
+
+it(`should find gastro header`,() => {
+    let doc = [
+        "01 Certain Infectious or Parasitic Diseases Start",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "       "
+    ];
+    let res = findParentSectionFromLinePosition(doc,doc.length-1,getTokenLayout());
+    expect((<ICDSection>res).completionItem.label).toBe(gastro.completionItem.label);
+
+});
+
+it(`should find gastro header`,() => {
+    let doc = [
+        "01 Certain Infectious or Parasitic Diseases Start",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "      Bacterial Foodborne Intoxications Start",
+        "          ",
+        "      End",
+        "      "
+    ];
+    let res = findParentSectionFromLinePosition(doc,doc.length-1,getTokenLayout());
+    expect((<ICDSection>res).completionItem.label).toBe(gastro.completionItem.label);
+
+});
+
+it(`should find chapter 1 header`,() => {
+    let doc = [
+        "01 Certain Infectious or Parasitic Diseases Start",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "      Bacterial Foodborne Intoxications Start",
+        "          ",
+        "      End",
+        "      Bacterial Intestinal Infections Start",
+        "          ",
+        "      End ",
+        "   End",
+        "   "
+    ];
+    let res = findParentSectionFromLinePosition(doc,doc.length-1,getTokenLayout());
+    expect((<ICDSection>res).completionItem.label).toBe(zeroOne.completionItem.label);
+
+});
+
+it(`should find foodborne header`,() => {
+    let doc = [
+        "01 Certain Infectious or Parasitic Diseases Start",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "       Bacterial Foodborne Intoxications Start",
+        "           - 1A10 Foodborne Staphylococcal Intoxication"
+    ];
+    let res = findParentSectionFromLinePosition(doc,doc.length-1,getTokenLayout());
+    expect((<ICDSection>res).completionItem.label).toBe(foodBorne.completionItem.label);
+
+});
+
+it(`should not find anything`,() => {
+    let doc = [
+        "01 Certain Infectious or Parasitic Diseases Start",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "      Bacterial Foodborne Intoxications Start",
+        "           - 1A10 Foodborne Staphylococcal Intoxication",
+        "      End",
+        "      Bacterial Intestinal Infections Start",
+        "          ",
+        "      End ",
+        "   End",
+        "End"
+    ];
+    let res = findParentSectionFromLinePosition(doc,doc.length-1,getTokenLayout());
+    expect((<ICDSection>res)).toBe(undefined);
+
+});
+
+it(`should not find anything`,() => {
+    let doc = [
+        "01 Certain Infectious or Parasitic Diseases Start",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "      Bacterial Foodborne Intoxications Start",
+        "           - 1A10 Foodborne Staphylococcal Intoxication",
+        "      End",
+        "      Bacterial Intestinal Infections Start",
+        "          ",
+        "      End ",
+        "   End",
+        "End",
+        ""
+    ];
+    let res = findParentSectionFromLinePosition(doc,doc.length-1,getTokenLayout());
+    expect((<ICDSection>res)).toBe(undefined);
 
 });
