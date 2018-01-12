266b9fd27c7a0df048643dad487d79a932ab0be6
Chris Gibb <chris.gibb@outlook.com>
2018-01-11 21:20:48 -0500

    started ast rule checks

---
+++ __tests__/AST/unknownTokenName.test.ts
@@ -0,0 +1,22 @@
+/// <reference types="ts-jest" />
+
+import {findSectionHeader} from "./../../src/req/icdToken";
+import {buildTokenLayout} from "./../../src/req/treeLayout";
+import {ZeroOne} from "./../../src/req/sections/01";
+import {Gastro} from "./../../src/req/sections/01/gastro";
+import {$1A10} from "./../../src/req/sections/01/1A10"
+
+let zeroOne = new ZeroOne();
+let gastro = new Gastro();
+let $1a10 = new $1A10();
+
+
+it(`should find token in section`,() => {
+    let res = findSectionHeader(zeroOne,gastro.completionItem.label);
+    expect(res!.completionItem.label).toBe(gastro.completionItem.label);
+});
+
+it(`should find token in section`,() => {
+    let res = findSectionHeader(zeroOne,$1a10.completionItem.label);
+    expect(res!.completionItem.label).toBe($1a10.completionItem.label);
+});
