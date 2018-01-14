3bc19f3b42e8f88b2274eec6aebd447b10a12ea1
Chris Gibb <chris.gibb@outlook.com>
2018-01-13 23:37:29 -0500

    fixed test

--- __tests__/autoComplete/autoCompleteAST.test.ts
+++ __tests__/autoComplete/autoCompleteAST.test.ts
@@ -3,22 +3,22 @@
 import {findTokenFromUnknownStart,findToken, ICDSection, ICDItem,ICDAttributes, ICDCompletionItem} from "./../../src/req/icdToken";
 import {getTokenLayout,buildMonarchTokens} from "./../../src/req/treeLayout";
 import {autoComplete} from "./../../src/req/autoComplete";
+import {ICDTokenID} from "../../src/req/icdTokenID";
 
 let tokens = buildMonarchTokens(getTokenLayout());
 for(let i = 0; i != tokens.length; ++i)
 {
     //ensure every token in the AST layout is actually reachable
-    it(`should be able to autocomplete ${tokens[i][2]}`,() => {
+    it(`should be able to autocomplete "${tokens[i][2]}" and its siblings`,() => {
         if((<string>tokens[i][2]))
         {
             let res : ICDSection | ICDItem | undefined = findTokenFromUnknownStart(getTokenLayout(),(<string>tokens[i][2]));
             let current : ICDAttributes = res!;
             let doc : Array<string> = new Array<string>();
-            if(!current.parent)
-            {
-                doc.push(current.completionItem.label+" Start");
-            }
-            else
+            
+            //assemble a document consisting of all of the parent sections leading to token[i]
+            doc.push(" ");
+            if(current.parent)
             {
                 while(current.parent)
                 {
@@ -26,10 +26,50 @@ for(let i = 0; i != tokens.length; ++i)
                     current = current.parent;
                 }
             }
+            //flip upside down, into the right order
             doc = doc.reverse();
-            console.log(doc);
+            
+            //autocompleted result
             let completed : void | Array<ICDCompletionItem> = autoComplete(doc.join("\n"));
-            expect(completed).toBe([(<ICDSection>res!).completionItem]);
+
+            //expected autocomplete result
+            let siblings : Array<ICDCompletionItem> = new Array<ICDCompletionItem>();
+
+            //not at the top of the tree
+            //should autcomplete all siblings at the current level we're at
+            if(res!.parent)
+            {
+                if((<ICDSection>res!.parent).childSections)
+                {
+                    for(let i = 0; i != (<ICDSection>res!.parent).childSections.length; ++i)
+                    {
+                        siblings.push((<ICDSection>res!.parent).childSections[i].completionItem);
+                    }
+                }
+
+                if((<ICDSection>res!.parent).childItems)
+                {
+                    for(let i = 0; i != (<ICDSection>res!.parent).childItems.length; ++i)
+                    {
+                        siblings.push((<ICDSection>res!.parent).childItems[i].completionItem);
+                    }
+                }
+            }
+
+            //at the top of the tree
+            //should only autocomplete for the topmost chapter headers
+            else
+            {
+                for(let k = 0; k != tokens.length; ++k)
+                {
+                    if(tokens[k][1] == <ICDTokenID>"icd11.SectionTopHeader")
+                    {
+                        let fullToken : ICDSection | ICDItem | undefined = findTokenFromUnknownStart(getTokenLayout(),(<string>tokens[k][2]));
+                        siblings.push(fullToken!.completionItem);
+                    }
+                }
+            }
+            expect(completed).toEqual(siblings);
         }
     });
 }
