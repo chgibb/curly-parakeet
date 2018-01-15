1a7537e4a3eea4c40eac56a62010a3554afdcecc
Chris Gibb <chris.gibb@outlook.com>
2018-01-15 15:27:09 -0500

    added tests for duplicate tokens

--- __tests__/documentValidation/duplicateToken.test.ts
+++ __tests__/documentValidation/duplicateToken.test.ts
@@ -14,3 +14,35 @@ it(`should not allow duplicate tokens`,() => {
 
     expect(res.code).toBe(DocumentStatusCode.DuplicateToken);
 });
+
+it(`should not allow duplicate tokens`,() => {
+    let doc = [
+        "01 Certain Infectious or Parasitic Diseases Start",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "   End",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "   End",
+        "End"
+    ].join("\n");
+
+    let res : DocumentStatus = validate(doc);
+
+    expect(res.code).toBe(DocumentStatusCode.DuplicateToken);
+});
+
+it(`should not allow duplicate tokens`,() => {
+    let doc = [
+        "01 Certain Infectious or Parasitic Diseases Start",
+        "   Gastroenteritis and Colitis of Infectious Origin Start",
+        "       Bacterial Foodborne Intoxications Start",
+        "           - 1A10 Foodborne Staphylococcal Intoxication",
+        "           - 1A10 Foodborne Staphylococcal Intoxication",
+        "       End",
+        "   End",
+        "End"
+    ].join("\n");
+
+    let res : DocumentStatus = validate(doc);
+
+    expect(res.code).toBe(DocumentStatusCode.DuplicateToken);
+});
