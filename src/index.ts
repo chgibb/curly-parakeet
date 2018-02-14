1e8d1cdf290f619a95d3a42379169365234540b8
Chris Gibb <chris.gibb@outlook.com>
2018-02-14 12:57:53 -0500

    broke editor loading into function

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,11 @@
-/// <reference path="./../node_modules/monaco-editor/monaco.d.ts" />
-
-import {MonacoLoader} from "./req/monacoLoader";
-
-import {validate} from "./req/validate";
-import {autoComplete} from "./req/autoComplete";
-import {getICDTokenColouring} from "./req/icdTokenColouring";
-import {getTokenLayout,buildMonarchTokens} from "./req/treeLayout"
+import {loadICDEditor} from "./req/loadICDEditor";
 
 (async function(){
-    let loader : MonacoLoader = new MonacoLoader();
-    await loader.loadMonaco();
 
-    monaco.languages.register({
-        id : "icd11Language"
-    });
-
-    monaco.languages.setMonarchTokensProvider('icd11Language', <monaco.languages.IMonarchLanguage>{
-        tokenizer : {
-            root : buildMonarchTokens(getTokenLayout(),false)
-        },
-        tokenPostfix : "."
-    });
-
-    monaco.languages.registerCompletionItemProvider("icd11Language",{
-        provideCompletionItems : function(model : monaco.editor.IReadOnlyModel,position : monaco.Position){
-            try
-            {
-                let textUntilPosition = model.getValueInRange({ startLineNumber: 1, startColumn: 1, endLineNumber: position.lineNumber, endColumn: position.column });
-                console.log("textUntilPosition");
-                console.log(textUntilPosition);
-                let item = autoComplete(textUntilPosition);
-                if(item)
-                    return item;
-            }
-            catch(err){}
-            return [];
-        }
-    });
-
-    // Define a new theme that constains only rules that match this language
-    monaco.editor.defineTheme('icd11Theme',{
-        base: 'vs',
-        inherit: false,
-        rules: getICDTokenColouring(),
-        colors : {}
-    });
-    
-    let editor : monaco.editor.IStandaloneCodeEditor = monaco.editor.create(document.getElementById('container')!, {
-        theme: 'icd11Theme',
-        value: "",
-        language: 'icd11Language',
-        autoIndent : true,
-        folding : true,
-        
-    });
-
-    setInterval(function(){
-        let status = validate(editor.getValue());
-        document.getElementById("documentStatus")!.innerHTML = `${status.code ? `Error ${status.code}: ` : ""} ${status.more}`;
-    },1000);
+    await loadICDEditor(
+        document.getElementById("container"),
+        document.getElementById("documentStatus")
+    );
 
 })().catch((err) => {
     throw err;
---
+++ src/req/loadICDEditor.ts
@@ -0,0 +1,79 @@
+/// <reference path="./../../node_modules/monaco-editor/monaco.d.ts" />
+
+import {MonacoLoader} from "./monacoLoader";
+
+import {validate} from "./validate";
+import {autoComplete} from "./autoComplete";
+import {getICDTokenColouring} from "./icdTokenColouring";
+import {getTokenLayout,buildMonarchTokens} from "./treeLayout"
+import { error } from "util";
+
+let errorMonitor : NodeJS.Timer;
+
+export function stopErrorMonitor() : void
+{
+    clearTimeout(errorMonitor);
+}
+
+export function loadICDEditor(div : HTMLElement | null,errorOutput : HTMLElement | null) : Promise<monaco.editor.IStandaloneCodeEditor>
+{
+    if(!div)
+        throw new Error("Editor container is null");
+    if(!errorOutput)
+        throw new Error("Error output container is null");
+
+    return new Promise<monaco.editor.IStandaloneCodeEditor>(async (resolve) => {
+        let loader : MonacoLoader = new MonacoLoader();
+        await loader.loadMonaco();
+
+        monaco.languages.register({
+            id : "icd11Language"
+        });
+
+        monaco.languages.setMonarchTokensProvider('icd11Language', <monaco.languages.IMonarchLanguage>{
+            tokenizer : {
+                root : buildMonarchTokens(getTokenLayout(),false)
+            },
+            tokenPostfix : "."
+        });
+
+        monaco.languages.registerCompletionItemProvider("icd11Language",{
+            provideCompletionItems : function(model : monaco.editor.IReadOnlyModel,position : monaco.Position){
+                try
+                {
+                    let textUntilPosition = model.getValueInRange({ startLineNumber: 1, startColumn: 1, endLineNumber: position.lineNumber, endColumn: position.column });
+                    console.log("textUntilPosition");
+                    console.log(textUntilPosition);
+                    let item = autoComplete(textUntilPosition);
+                    if(item)
+                        return item;
+                }
+                catch(err){}
+                return [];
+            }
+        });
+
+        monaco.editor.defineTheme('icd11Theme',{
+            base: 'vs',
+            inherit: false,
+            rules: getICDTokenColouring(),
+            colors : {}
+        });
+    
+        let editor : monaco.editor.IStandaloneCodeEditor = monaco.editor.create(div!, {
+            theme: 'icd11Theme',
+            value: "",
+            language: 'icd11Language',
+            autoIndent : true,
+            folding : true,
+        
+        });
+
+        errorMonitor = setInterval(function(){
+            let status = validate(editor.getValue());
+            document.getElementById(errorOutput.id)!.innerHTML = `${status.code ? `Error ${status.code}: ` : ""} ${status.more}`;
+        },1000);
+
+        resolve(editor);
+    }); 
+}
