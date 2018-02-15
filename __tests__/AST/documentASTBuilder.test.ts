f1fd89e045dd4bf93e2cd43c0f7d90d07e28770f
Chris Gibb <chris.gibb@outlook.com>
2018-02-15 11:00:57 -0500

    added generated tests for document ast traversal

--- __tests__/AST/documentASTBuilder.test.ts
+++ __tests__/AST/documentASTBuilder.test.ts
@@ -1,7 +1,8 @@
 /// <reference types="ts-jest" />
 
-import {ICDSection,ICDGenericToken} from "./../../src/req/icdToken";
+import {ICDSection,ICDGenericToken, findTokenFromUnknownStart, ICDItem, ICDAttributes} from "./../../src/req/icdToken";
 import {buildDocumentAST,copyAndStripChildren,copyToken} from "./../../src/req/documentAST";
+import {getTokenLayout,buildMonarchTokens} from "./../../src/req/treeLayout";
 
 it(`should build correct AST for document 1`,() => {
     let doc = [
@@ -74,3 +75,64 @@ it(`should build correct AST for document 2`,() => {
     expect((<ICDSection>res[2]).childSections.length).toEqual(0);
     expect((<ICDSection>res[2]).childItems.length).toEqual(0);
 });
+
+let tokens = buildMonarchTokens(getTokenLayout(),true);
+for(let i = 0; i != tokens.length; ++i)
+{
+    it(`should be able to traverse reference AST the same as AST built from document for "${tokens[i][2]}"`,() => {
+        if((<string>tokens[i][2]))
+        {
+            let current = (<ICDSection | ICDItem>findTokenFromUnknownStart(
+                getTokenLayout(),
+                (<string>tokens[i][2])
+            ));
+
+            let doc : Array<string> = new Array<string>();
+
+            doc.push(" ");
+            if(current.tokenType == "icd11.SectionTopHeader" || current.tokenType == "icd11.SectionHeader")
+            {
+                doc.push(current.completionItem.label+" Start");
+            }
+
+            if(current.tokenType == "icd11.item")
+                doc.push(current.completionItem.label);
+
+            if(current.parent)
+            {
+                while(current!.parent)
+                {
+                    doc.push(current.parent!.completionItem.label+" Start");
+                    current = (<ICDSection | ICDItem>current!.parent);
+                }
+            }
+            //flip upside down, into the right order
+            doc = doc.reverse();
+
+            let docAST = buildDocumentAST(doc.join("\n"));
+
+            expect(findTokenFromUnknownStart(docAST,(<string>tokens[i][2]))).toBeDefined();
+
+            let referenceToken = (<ICDSection | ICDItem>findTokenFromUnknownStart(
+                getTokenLayout(),
+                (<string>tokens[i][2])
+            ));
+
+            let docToken = (<ICDSection | ICDItem>findTokenFromUnknownStart(
+                docAST,
+                (<string>tokens[i][2])
+            ));
+
+            if(referenceToken.parent && docToken.parent)
+            {
+                while(referenceToken.parent && docToken.parent)
+                {
+                    expect(docToken.completionItem.label).toEqual(referenceToken.completionItem.label);
+                    docToken = (<ICDSection | ICDItem>docToken.parent);
+                    referenceToken = (<ICDSection | ICDItem>referenceToken.parent);
+                }
+            }
+
+        }
+    });
+}
