756cff55482bc197d98b77619b9b26fe8ae56a90
Chris Gibb <chris.gibb@outlook.com>
2018-02-22 15:02:54 -0500

    removed mongo

--- scripts/runDBDaemon.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-(set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
-
-./db/bin/mongod --dbpath db/db
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,11 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as session from "express-session";
-const connectMongo = require("connect-mongo");
-const mongoose = require("mongoose");
 
 import {userModel,AuthenticateUser} from "./server/user";
 import {LoginRequest} from "./req/loginRequest";
 
-const mongoStore = connectMongo(session);
-
 const app = express();
-
-mongoose.connect("mongodb://localhost:27017");
-
-const db = mongoose.connection;
-
-db.on('error',console.error.bind(console,'connection error:'));
-
-db.once('open',function(){
-    console.log("connected to db");
-});
-
-app.use(session({
-    secret : "curly",
-    resave : true,
-    saveUninitialized : false,
-    store : new mongoStore({
-        mongooseConnection : db
-    })
-}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
