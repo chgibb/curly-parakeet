5b643ab443f62cce3ac74274bb99b76378e45245
Chris Gibb <chris.gibb@outlook.com>
2018-02-23 13:17:04 -0500

    changed to using dedicated record type

--- src/server/store/store.ts
+++ src/server/store/store.ts
@@ -11,23 +11,93 @@ interface Record<T>
     item : T;
 }
 
-export function find<T>(storePath : string,comp : (item : T) => boolean) : Promise<T | undefined>
+export function find<T>(storePath : string,comp : (item : T) => boolean) : Promise<Record<T> | undefined>
 {
-    return new Promise<T | undefined>((resolve,reject) => {
+    return new Promise<Record<T> | undefined>((resolve,reject) => {
         if(!fs.existsSync(storePath))
             return resolve(undefined);
+
+        try
+        {
+            let store : Array<Record<T>> = JSON.parse(fs.readFileSync(storePath).toString());
+
+            for(let i = 0 ; i != store.length; ++i)
+            {
+                let res = comp(store[i].item);
+                if(res)
+                    resolve(store[i]);
+            }
+
+        }
+        catch(err)
+        {
+            resolve(undefined);
+        }
+
+        resolve(undefined);
     });
 }
 
-export function write<T>(storePath : string,item : T) : boolean
+export function writeRecord<T>(storePath : string,record : Record<T>) : boolean
 {
     let components : path.ParsedPath = path.parse(storePath);
-    mkdirp.sync(components.dir);
 
     if(!fs.existsSync(storePath))
     {
         try
         {
+            mkdirp.sync(components.dir);
+            let store : Array<Record<T>> = new Array<Record<T>>();
+            store.push(<Record<T>>{
+                id : makeHash(JSON.stringify(record.item)),
+                item : record.item
+            });
+
+            fs.writeFileSync(storePath,JSON.stringify(store));
+
+        }
+        catch(err)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    let updatedRecord = false;
+    try
+    {
+        let store : Array<Record<T>> = JSON.parse(fs.readFileSync(storePath).toString());
+
+        for(let i = 0; i != store.length; ++i)
+        {
+            if(store[i].id == record.id)
+            {
+                store[i].id = makeHash(JSON.stringify(record.item));
+                store[i].item = record.item;
+                updatedRecord = true;
+                break;
+            }
+        }
+        fs.writeFileSync(storePath,JSON.stringify(store));
+    }
+    catch(err)
+    {
+        return false;
+    }
+
+    return updatedRecord;
+}
+
+export function write<T>(storePath : string,item : T) : boolean
+{
+    let components : path.ParsedPath = path.parse(storePath);
+
+    if(!fs.existsSync(storePath))
+    {
+        try
+        {
+            mkdirp.sync(components.dir);
             let store : Array<Record<T>> = new Array<Record<T>>();
             store.push(<Record<T>>{
                 id : makeHash(JSON.stringify(item)),
@@ -51,14 +121,10 @@ export function write<T>(storePath : string,item : T) : boolean
 
         let itemID = makeHash(JSON.stringify(item));
 
-        for(let i = 0; i != store.length; ++i)
-        {
-            if(store[i].id == itemID)
-            {
-                store[i].item = item;
-                break;
-            }
-        }
+        store.push(<Record<T>>{
+            id : makeHash(JSON.stringify(item)),
+            item : item
+        });
 
         fs.writeFileSync(storePath,JSON.stringify(store));
     }
@@ -66,6 +132,5 @@ export function write<T>(storePath : string,item : T) : boolean
     {
         return false;
     }
-
     return true;
 }
