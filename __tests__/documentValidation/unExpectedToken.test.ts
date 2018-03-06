38b28e2573e39df585604131f1faf55f7d3d110e
Chris Gibb <chris.gibb@outlook.com>
2018-03-06 10:31:53 -0500

    added unexpected token test

--- __tests__/documentValidation/unExpectedToken.test.ts
+++ __tests__/documentValidation/unExpectedToken.test.ts
@@ -2,10 +2,24 @@
 
 import {validate,DocumentStatus,DocumentStatusCode} from "./../../src/req/editor/validate";
 
-it(`should be unexpected token`,() => {
+it(`should be unexpected token 1`,() => {
     let doc = [
         "01 Certain Infectious or Parasitic Diseases Start",
         "   Bacterial Foodborne Intoxications Start",
+        "   End",
+        "End"
+    ].join("\n");
+
+    let res : DocumentStatus = validate(doc);
+    expect(res.code).toBe(DocumentStatusCode.UnExpectedToken);
+});
+
+it(`should be unexpected token 2`,() => {
+    let doc = [
+        "00 Patient Information Start",
+        "   - First Name: John",
+        "   - Last Name: Smith",
+        "   01 ICD Coding Start",
         "   End",
         "End"
     ].join("\n");
