278a796b5d6fc61380e575547b23ef5f7e668725
Chris Gibb <chris.gibb@outlook.com>
2018-03-08 09:46:04 -0500

    added test

--- __tests__/documentValidation/incompleteSection.test.ts
+++ __tests__/documentValidation/incompleteSection.test.ts
@@ -17,3 +17,21 @@ it(`should not allow incomplete section 1`,() => {
 
     expect(res.code).toBe(DocumentStatusCode.IncompleteSection);
 });
+
+it(`should not allow incomplete section 1`,() => {
+    let doc = [
+        "02 Referrals Start",
+        "   Referral Start",
+        "      - To: Dr. Mago",
+        "       - Type: Computing",
+        "   End",
+        "   Referral Start",
+        "       - To: Dr. Benson",
+        "   End",
+        "End"
+    ];
+
+    let res = validate(doc.join("\n"));
+
+    expect(res.code).toBe(DocumentStatusCode.IncompleteSection);
+});
