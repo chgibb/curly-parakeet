abc1bf7d23f716e81581bcbf7828a1d4f95aac55
Chris Gibb <chris.gibb@outlook.com>
2018-03-08 13:22:37 -0500

    fixed ret code passing

--- scripts/test.bash
+++ scripts/test.bash
@@ -2,12 +2,20 @@
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
 
 function cleanArtifacts {
-    rm -rf db
+    rm -rf db/
+    rmdir db
     rm -rf rwTest
+    rmdir rwTest
 }
 
-./node_modules/.bin/tsc && ./node_modules/.bin/jest
+cleanArtifacts
 
+./node_modules/.bin/tsc -p tsconfig.json
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+./node_modules/.bin/jest
 returnCode=$?
 
 cleanArtifacts
